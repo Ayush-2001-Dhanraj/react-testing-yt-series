c1740806d04b88c78ea31b3d22499133367f2a4d
Ayush4NattyHatty <ayushdhanraj@nattyhattyl.com>
2023-01-26 13:50:05 +0530

    add 'describe'

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,16 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders with Hello", () => {
-  render(<Greet />);
-  const greetEl = screen.getByTestId("greet-msg");
-  expect(greetEl).toBeInTheDocument();
-  expect(greetEl.textContent).toBe("Hello");
-});
+describe("Greet", () => {
+  test("renders with Hello", () => {
+    render(<Greet />);
+    const greetEl = screen.getByTestId("greet-msg");
+    expect(greetEl).toBeInTheDocument();
+    expect(greetEl.textContent).toBe("Hello");
+  });
 
-test("Greet renders with name", () => {
-  render(<Greet name="Ayush" />);
-  const greetEl = screen.getByTestId("greet-msg");
-  expect(greetEl).toBeInTheDocument();
-  expect(greetEl.textContent).toBe("Hello Ayush");
+  test("renders with name", () => {
+    render(<Greet name="Ayush" />);
+    const greetEl = screen.getByTestId("greet-msg");
+    expect(greetEl).toBeInTheDocument();
+    expect(greetEl.textContent).toBe("Hello Ayush");
+  });
 });
